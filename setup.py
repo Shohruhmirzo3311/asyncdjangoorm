ea1eea3877403adf97a16bceb20b66121506c21b
Shohruhmirzo3311 <jamoliddinovshohruh1@gmail.com>
2025-09-25 21:17:38 +0500

    upgraded to 0.1.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="asyncdjangoorm",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(exclude=["tests*", "asyncdjangoorm._internal*"]),
     entry_points={
         "console_scripts": [
