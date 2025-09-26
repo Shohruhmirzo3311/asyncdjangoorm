b298096c3b009b06237cc5b9b747c065189bd24e
Shohruhmirzo3311 <jamoliddinovshohruh1@gmail.com>
2025-09-26 08:31:10 +0500

    changed mismatches

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="asyncdjangoorm",
-    version="0.2.0",
+    version="0.2.1",
     entry_points={
         "console_scripts": [
             "asyncdjangoorm-admin=asyncdjangoorm.__main__:main",
